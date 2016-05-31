9cadac4fa8ee79b46bfa5ad453416834d32eb3cc
Loic Esteve <loic.esteve@ymail.com>
2016-05-31 14:04:34 +0200

    Use ifmain guards for Windows

--- work/lru_writer.py
+++ work/lru_writer.py
@@ -3,5 +3,6 @@ from joblib import Parallel, delayed
 from my_module import my_function
 
 
-while True:
-    Parallel(n_jobs=-1)(delayed(my_function)(i) for i in range(4))
+if __name__ == '__main__':
+    while True:
+        Parallel(n_jobs=-1)(delayed(my_function)(i) for i in range(4))
